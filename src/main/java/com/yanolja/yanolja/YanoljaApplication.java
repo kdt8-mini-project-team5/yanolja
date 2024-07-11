ed31cceec31ea759d8d281e15e9a740847b89059
Domae-back-end <ty_ty123@naver.com>
2024-07-11 18:21:02 +0900

    init

---
+++ src/main/java/com/yanolja/yanolja/YanoljaApplication.java
@@ -0,0 +1,13 @@
+package com.yanolja.yanolja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanoljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanoljaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanolja/yanolja/YanoljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanolja.yanolja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanoljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
