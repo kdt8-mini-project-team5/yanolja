6038dc4e0466e22604b842edb54b99f8fbaa33fc
sinsj5858 <goddnjswns58@gmail.com>
2024-07-16 14:58:56 +0900

    feat : validException 추가

--- src/main/java/com/yanolja/yanolja/global/handler/exception/HandlerException.java
+++ src/main/java/com/yanolja/yanolja/global/handler/exception/HandlerException.java
@@ -1,9 +1,12 @@
 package com.yanolja.yanolja.global.handler.exception;
 
 import com.yanolja.yanolja.global.model.response.ErrorResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -20,5 +23,18 @@ public class HandlerException {
 
         return ResponseEntity.status(e.getStatusCode())
             .body(new ErrorResponse(e.getStatusText()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationExceptions(
+        MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .toList();
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(String.join(", ", errors)));
     }
 }
